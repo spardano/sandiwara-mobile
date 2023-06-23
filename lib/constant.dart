2cbfc75af43246a4ff4aaf737542925e156f3df2
Ridho Al Amzah <58523131+ridho15@users.noreply.github.com>
2023-06-23 16:29:55 +0700

    Upate

--- lib/constant.dart
+++ lib/constant.dart
@@ -2,8 +2,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
-const String apiUrl = 'http://192.168.100.86:8000/api';
-const String mainUrl = 'http://192.168.100.86:8000';
+const String apiUrl = 'http://192.168.100.38:8000/api';
+const String mainUrl = 'http://192.168.100.38:8000';
 
 InAppWebViewGroupOptions options = InAppWebViewGroupOptions(
     crossPlatform: InAppWebViewOptions(
